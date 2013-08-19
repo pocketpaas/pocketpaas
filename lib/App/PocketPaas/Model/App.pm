aff9cedcf7c4b82b1409f1c60997d32b1ad55045
Nate Jones <nate@endot.org>
2013-08-19 14:17:58 +0000

    remove example comments

--- lib/App/PocketPaas/Model/App.pm
+++ lib/App/PocketPaas/Model/App.pm
@@ -16,16 +16,6 @@ sub load {
 
     my ( @images, @containers );
 
-# {
-#   'Id' => '8a79577a855d42713158aaaeff29552b765e07748ade5e5172d8a2814e614448',
-#   'Status' => 'Up 13 minutes',
-#   'Image' => 'pocketapp/testapp:run-2013-07-24-03-28-15',
-#   'Ports' => '49154->5000',
-#   'Command' => '/start web',
-#   'SizeRw' => 0,
-#   'Created' => 1374714125,
-#   'SizeRootFs' => 0
-# }
     foreach my $docker_container (@$docker_containers) {
         if ( $docker_container->{Image} =~ m{^pocketapp/$name:run-([\d-]+)$} )
         {
@@ -53,15 +43,6 @@ sub load {
                 );
         }
     }
-
-# {
-#   'Size' => 12288,
-#   'Id' => '45d1d2933f039f2c7f647bb618ecaf612dc32d2a1ddd10a4ffafdaf6dd6e03a4',
-#   'VirtualSize' => 679359403,
-#   'Created' => 1374628038,
-#   'Tag' => 'build-2013-07-24-00-58-11',
-#   'Repository' => 'pocketapp/testapp'
-# },
 
     my $potential_tags = {};
     foreach my $docker_image (@$docker_images) {
