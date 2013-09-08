a419ea822f204a92494635ded23da9678918f6b4
Nate Jones <nate@endot.org>
2013-09-08 16:05:57 -0700

    adding perltidy script

---
+++ tidy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO check for carton and fall back to system version of perltidy if not
+# found.  make sure perltidy version is 20130806
+
+for file in `./.ack -f --perl --ignore-file=is:.ack --ignore-dir local`; do
+    echo "Tidying $file";
+    carton exec perltidy -pbp -b -nse -nst $file;
+done
